7c937fbb4e4c0ffe55159f1477ad1b2b769d6c16
Andrew Byzhynar <abyzhynar@luxoft.com>
2019-12-10 13:47:49 +0200

    Initial commit

--- iproject.h
+++ iproject.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <string>
+
 namespace dev{
 
 class IProject {
   virtual int run() = 0;
+  virtual void user_input() = 0;
+  virtual float parse_input() const = 0;
+  virtual void display_result(const std::string res) const = 0;
+  virtual std::string get_result() const = 0;
+  virtual std::string get_user_input() const = 0;
 };
 
 }
--- main.cc
+++ main.cc
@@ -1,5 +1,15 @@
 #include "project.h"
+#include <iostream>
 
 int main() {
+
+  dev::Project obj;
+  std::cout<< "Input string in format: \"number\"\"space\"\"number\"\"operation\"" << std::endl;
+  obj.user_input();
+  std::cout << obj.get_user_input() << std::endl;
+
+
+  std::string res = obj.parse_input();
+
   return 0;
 }
