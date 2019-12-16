b18cff046626dc9b0b39f0759c142a73f7fe16d5
Alexander Kutsan <akutsan@luxoft.com>
2019-12-16 17:55:31 +0000

    Day3

--- run.cpp
+++ run.cpp
@@ -5,67 +5,125 @@
 #include <iomanip>
 #include <sstream>
 
-void run(std::istream& in, std::ostream& out){
+
+struct Movie {
+  int index;
+  std::string name;
+  std::string type;
+};
+
+struct Rental {
+  Movie movie;
+  int days_rented;
+
+
+  int GetFrequentRenterPoints() {
+    if (movie.type == "NEW_RELEASE" && days_rented > 1) {
+      return 2;
+    }
+    return 1;
+  }
+
+
+  double GetAmount() const {
+    if (movie.type == "NEW_RELEASE") {
+          return days_rented * 3;
+    }
+    if (movie.type == "REGULAR") {
+      if (days_rented > 2) {
+        return 2 + (days_rented - 2) * 1.5;
+      }
+      return 2;
+    }
+    if (movie.type == "CHILDRENS") {
+        if (days_rented > 3) {
+          return 1.5 + (days_rented - 3) * 1.5;
+        }
+        return 1.5;
+    }
+    return 0;
+  }
+};
+
+ std::vector<std::string> Split(const std::string& line, const char delimeter) {
+   std::vector<std::string> movie;
+  for (size_t first=0, last=0; last < line.length(); first=last+1) {
+    last = line.find(delimeter, first);
+    movie.push_back(line.substr(first, last-first));
+  }
+  return movie;
+}
+
+ std::vector<std::string> UserInput(std::istream& in) {
+   std::vector<std::string> user_input;
+   while (true) {
+     std::string line;
+     std::getline(in, line);
+     if (line.empty()) {
+       break;
+     }
+     user_input.push_back(line);
+   }
+   return user_input;
+ }
+
+ struct MovieRepo {
+    MovieRepo(std::ifstream& db_file) {
+      for (std::string line; std::getline(db_file, line);) {
+        auto movie_data = Split(line, ';');
+        Movie movie {std::stoi(movie_data[0]), movie_data[1], movie_data[2]};
+        movies_.insert(std::make_pair(movie.index, movie));
+      }
+    }
+
+    Movie getMovie(int index) const {
+        return movies_.at(index);
+    }
+
+     void print(std::ostream& out) const {
+        for (const auto& movie: movies_) {
+            out << movie.first<< ": " << movie.second.name<< "\n";
+        }
+     }
+
+ private:
+    std::map<int, Movie> movies_;
+ };
+
+ void run(std::istream& in, std::ostream& out){
   using namespace std::literals;
   // read movies from file
   std::ifstream movieStream{"movies.csv"};
-  std::map<int, std::vector<std::string>> movies{};
-  for (std::string line; std::getline(movieStream, line);) {
-    std::vector<std::string> movie;
-    for (size_t first=0, last=0; last < line.length(); first=last+1) {
-      last = line.find(';', first);
-      movie.push_back(line.substr(first, last-first));
-    }
-    movies.insert(std::make_pair(std::stoi(movie[0]), movie));
-    out << movie[0] << ": " << movie[1] << "\n";
-  }
+  MovieRepo movie_repo (movieStream);
+  movie_repo.print(out);
+
   out << "Enter customer name: ";
   std::string customerName;
   getline(in, customerName);
 
   out << "Choose movie by number followed by rental days, just ENTER for bill:\n";
-  double totalAmount = 0;
-  int frequentRenterPoints = 0;
   std::ostringstream result;
   result << std::fixed << std::setprecision(1);
   result << "Rental Record for " + customerName + "\n";
-  while (true) {
-    std::string input;
-    std::getline(in, input);
-    if (input.empty()) {
-      break;
-    }
-    std::vector<std::string> rental;
-    for (size_t first=0, last=0; last < input.length(); first=last+1) {
-      last = input.find(' ', first);
-      rental.push_back(input.substr(first, last-first));
-    }
-    std::vector<std::string> movie = movies[std::stoi(rental[0])];
-    double thisAmount = 0;
-    int daysRented = std::stoi(rental[1]);
-    //determine amounts for rental
-    if (movie[2] == "REGULAR") {
-      thisAmount += 2;
-      if (daysRented > 2)
-        thisAmount += (daysRented - 2) * 1.5;
-    } else if (movie[2] == "NEW_RELEASE") {
-      thisAmount += daysRented * 3;
-    } else if (movie[2] == "CHILDRENS") {
-      thisAmount += 1.5;
-      if (daysRented > 3)
-        thisAmount += (daysRented - 3) * 1.5;
-    }
 
-    // add frequent renter points
-    frequentRenterPoints++;
-    // add bonus for a two day new release rental
-    if (movie[2] == "NEW_RELEASE" && daysRented > 1) {
-      frequentRenterPoints++;
-    }
-    // show figures for this rental
-    result << "\t" << movie[1] + "\t" << thisAmount << "\n";
-    totalAmount += thisAmount;
+  std::vector<std::string> user_input = UserInput(in);
+  double totalAmount = 0;
+  int frequentRenterPoints = 0;
+
+
+  for (const auto& line : user_input) {
+      std::vector<std::string> rental_data = Split(line, ' ');
+      Rental rental = {movie_repo.getMovie(std::stoi(rental_data[0])),
+                                std::stoi(rental_data[1])};
+
+      double thisAmount = rental.GetAmount();
+
+      frequentRenterPoints += rental.GetFrequentRenterPoints();
+      // show figures for this rental
+      result << "\t" << rental.movie.name + "\t" << thisAmount << "\n";
+      totalAmount += thisAmount;
   }
+
 
   // add footer lines
   result << "You owed " << totalAmount << "\n";
